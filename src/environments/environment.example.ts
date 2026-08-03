f996ac310c34db4c0d34e500ee5a5cb81c8db9d9
Hussein Menkam <93983437+yabainjump@users.noreply.github.com>
2026-08-03 01:03:03 +0100

    Ignore local environment configuration

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://backend.example.com/api',
+  allowDemoFallback: false,
+};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  // apiBaseUrl: 'https://backend.onehealthnetwork.yaba-in.com/api',
-  apiBaseUrl: 'https://backend.onehealthnetwork.yaba-in.com/api',
-  allowDemoFallback: true,
-};
